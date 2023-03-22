65bf7e227f1d19ebb5e8a1e652c7f50596ed362c
Askhat <73778760+verozroke@users.noreply.github.com>
2023-03-22 23:50:03 +0600

    sexual harassment from git

--- src/components/ThemeButton.vue
+++ src/components/ThemeButton.vue
@@ -17,7 +17,7 @@ function changeTheme() {
 }
 
 function applyTheme(themeName) {
-    document.querySelector('[title="theme"]').setAttribute('href', `/theme-${themeName}.css`);
+    document.querySelector('[title="theme"]').setAttribute('href', `/askatti-bugatti/theme-${themeName}.css`);
     document.querySelectorAll('.change__theme').forEach(button => {
         button.style.display = 'block';
     });
