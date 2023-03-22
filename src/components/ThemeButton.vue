98554efe1a9051cffefe919aba76da02daf2f09c
Askhat <73778760+verozroke@users.noreply.github.com>
2023-03-22 23:06:07 +0600

    wtf

--- src/components/ThemeButton.vue
+++ src/components/ThemeButton.vue
@@ -17,7 +17,7 @@ function changeTheme() {
 }
 
 function applyTheme(themeName) {
-    document.querySelector('[title="theme"]').setAttribute('href', `/src/styles/theme-${themeName}.scss`);
+    document.querySelector('[title="theme"]').setAttribute('href', `./src/styles/theme-${themeName}.scss`);
     document.querySelectorAll('.change__theme').forEach(button => {
         button.style.display = 'block';
     });
